de5a765c095fd6404cbbca14d8ce767c6f0b8840
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2023-05-10 18:31:01 -0400

    button component started and semi-styled

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
-};
+const Button = ({styles}) => {
+  return (
+    <button type="button" className={`py-4 px-6 bg-blue-gradient`}>
+      Get Started
+    </button>
+  )
+}
 
 export default Button;
