9d4bedb007416a8439889050d63442bc8c0db69b
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2023-05-11 14:14:36 -0400

    logos for clients added underneath testimonials

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styles from "../style"
+import { clients } from "../constants";
 
-const Clients = () => {
-  return <div>Clients</div>;
-};
+//here we want to map over our clients to show them on the landing page
+const Clients = () => (
+   <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+          <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain"/>
+        </div>
+      ))}
+    </div>
+   </section>
+);
 
 export default Clients;
